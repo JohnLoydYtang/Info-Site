596e05f1fefa64ee10bb733826f96b085ccc6dd7
Ytang, John Loyd M. <ytangjohnloyd@gmail.com>
2024-02-05 13:10:19 +0800

    Added Figure and Labels in App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,99 @@
+import NavBar from "../src/component/navbar/NavBar";
 import Header from "../src/parts/header/Header";
 import Accordion from 'react-bootstrap/Accordion';
+import Alert from 'react-bootstrap/Alert';
+import Figure from 'react-bootstrap/Figure';
+import Figure1 from "../src/assets/images/figures/figure-1.png";
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div >
+      <NavBar />
       <Header />
-      <div className="App">
-        <p>This is my React Info-Site!! <br />
-          Here we can see how i set-up my ReactJS Projects.
-        </p>
-      </div>
       <Accordion defaultActiveKey="0">
+        {/* Starting ReactJs Project */}
         <Accordion.Item eventKey="0">
+          <Accordion.Header><strong>Starting ReactJs Project</strong></Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+            <div className="Figure-css">
+              <Figure>
+                <Figure.Image
+                  width={400}
+                  height={180}
+                  alt="171x180"
+                  src={Figure1}
+                />
+                <Figure.Caption className="Figure-css">
+                  Nulla vitae elit libero, a pharetra augue mollis interdum.
+                </Figure.Caption>
+              </Figure>
+            </div>
+          </Accordion.Body>
+        </Accordion.Item>
+        {/* BootStrap Set-up */}
+        <Accordion.Item eventKey="1">
+          <Accordion.Header><strong>BootStrap Set-Up</strong></Accordion.Header>
+          <Accordion.Body>
+            <Alert variant="dark">Installation
+              <hr />
+              <p className="mb-0">
+                <code>npm install react-bootstrap bootstrap</code>
+              </p>
+            </Alert>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+            <Alert variant="dark"> /* The following line can be included in your src/index.js or App.js file */
+              <hr />
+              <p className="mb-0">
+                <code>import 'bootstrap/dist/css/bootstrap.css';</code>
+              </p>
+            </Alert>
+            Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        {/* Navigation */}
+        <Accordion.Item eventKey="2">
           <Accordion.Header><strong>Navigation / Routing</strong></Accordion.Header>
           <Accordion.Body>
+            <Alert variant="dark">Installation
+              <hr />
+              <p className="mb-0">
+                <code>npm install react-dom</code>
+              </p>
+            </Alert>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
             minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            aliquip ex ea commodo consequat.
+            <Alert variant="dark">Set-up (Put this in your index.js or other navigation file)
+              <hr />
+              <p className="mb-0">
+                <code>import BrowserRouter as Router, Routes, Route from react-router-dom </code>
+              </p>
+            </Alert>
+            Duis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="2">
-          <Accordion.Header>Accordion Item #3</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            pariatur. Excepteur sint occaecat cupidatat non proident,
+            <Alert variant="dark">Set-up (Put this in your index.js or other navigation file)
+              <hr />
+              <p className="mb-0">
+                <code>import BrowserRouter as Router, Routes, Route from react-router-dom </code>
+              </p>
+            </Alert>
+            sunt in
             culpa qui officia deserunt mollit anim id est laborum.
           </Accordion.Body>
         </Accordion.Item>
@@ -73,7 +122,7 @@ function App() {
           </Accordion.Body>
         </Accordion.Item>
       </Accordion>
-    </div>
+    </div >
   );
 }
 
--- src/parts/header/Header.js
+++ src/parts/header/Header.js
@@ -1,45 +1,14 @@
-import React from "react";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Image from 'react-bootstrap/Image';
-import ReactLogo from '../../assets/images/react-logo.png';
+import './Header.css';
 
 export default function header() {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="#home">
-                    <Image
-                        alt=""
-                        src={ReactLogo}
-                        roundedCircle
-                        fluid
-                        width="30"
-                        className="d-inline-block align-top"
-                    />{' '}
-                    Info-Site
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <div className="Header">
+            <p><br />
+                This is my React Info-Site!! <br />
+                Here we can see how i set-up my ReactJS Projects.<br />
+                This is my Starter React.
+                <br />
+            </p>
+        </div>
     );
 }
