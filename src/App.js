10062547874cac3c3bc4f46447fbd644f00ec3e1
Ytang, John Loyd M. <ytangjohnloyd@gmail.com>
2024-02-06 09:40:31 +0800

    Final Commit

--- src/App.js
+++ src/App.js
@@ -125,15 +125,7 @@ function App() {
         <Accordion.Item eventKey="3">
           <Accordion.Header><strong>Final</strong></Accordion.Header>
           <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-            Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident,
-            sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            Search for folder structure as the project goes big.
             <div className="Figure-css">
               <br />
               Then you are ready to code!!
